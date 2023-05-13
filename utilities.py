5ecaeadd89156279a4fe55c7536e9c7287d3634e
Savva Morozov <savva@mit.edu>
2023-05-13 18:04:38 -0400

    need to write by hand

---
+++ utilities.py
@@ -0,0 +1,21 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def unit_vector(i: int, n: int):
+    e_i = np.zeros(n)
+    e_i[i] = 1
+    return e_i
+
+
+def cross_2d(v1, v2):
+    return (v1[0] * v2[1] - v1[1] * v2[0])[0]
+
+
+def normalize_vec(vec: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    return vec / np.linalg.norm(vec)
+
+
+def two_d_rotation_matrix_from_angle(theta: float) -> npt.NDArray[np.float64]:
+    R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+    return R
